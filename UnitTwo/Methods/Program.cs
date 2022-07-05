674e3ca5f8b6f0f969b52edbe4f64cc074282e51
Aaron Krumsieg <a.krumsieg@gmail.com>
2022-07-05 15:07:23 -0600

    add material for methods, string formatting, and case conventions

---
+++ UnitTwo/Methods/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("Enter something: ");
+string input = Console.ReadLine();
+int num = -20;
+bool isNum = int.TryParse(input, out num);
+Console.WriteLine("isNum = " + isNum);
+Console.WriteLine("num = " + num);
+
+//Console.WriteLine("Do you like dogs? true/false");
+//bool trueOrFalse = bool.Parse(Console.ReadLine());
+//void PrintMessage (bool tOrF)
+//{
+//    Console.WriteLine($"Do you like dogs? {tOrF}");
+//}
+//PrintMessage(trueOrFalse);
+
+
+//void PrintMessage(string input)
+//{
+//    Console.WriteLine(input);
+//}
+//PrintMessage("hello");
+
+
+//Random random = new Random();
+//Console.WriteLine(random.Next());
+//Console.WriteLine(random.Next(10));
+//Console.WriteLine(random.Next(-100, 100)); //low range is inclusive, hi range is exclusive
+
+
+int numOne = 3;
+int numTwo = 4;
+
+static int AddTwoNumbersPlusOne(int a, out int b)
+{
+    b++;
+    return a + b;
+}
+
+int sum = AddTwoNumbersPlusOne(numOne, out numTwo);
+Console.WriteLine(sum);
+Console.WriteLine("numTwo = " + numTwo);
---
+++ UnitTwo/RandomNotes/Program.cs
@@ -0,0 +1,27 @@
+﻿//string writing syntaxes
+//CONCATENATION
+string name = "Aaron";
+string greeting = "Yo!";
+Console.WriteLine(greeting + " My name is " + name);
+
+//INTERPOLATION
+Console.WriteLine($"{greeting} My name is {name}");
+
+//COMPOSITE FORMATTING
+Console.WriteLine("{0} My name is {1}", greeting, name);
+
+
+//Casing Conventions
+// camelCasing - Variables
+int num = 9;
+string thisIsALongVariableName = "yep";
+
+//PascalCasing - Classes, Methods
+// ThisIsPascalCasing
+// Pascal
+
+//snake_case
+//this_is_snake_casing
+
+//kebab-casing
+//this-is-also-kebab-casing
