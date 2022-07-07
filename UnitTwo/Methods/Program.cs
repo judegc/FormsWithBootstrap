ed1a4eceac81e5e97015ef7fd2c73993e0c3f7e3
Aaron Krumsieg <a.krumsieg@gmail.com>
2022-07-06 18:59:23 -0600

    add string method examples

--- UnitTwo/Methods/Program.cs
+++ UnitTwo/Methods/Program.cs
@@ -1,4 +1,6 @@
-﻿//Console.WriteLine("Enter something: ");
+﻿using System.Text;
+
+//Console.WriteLine("Enter something: ");
 //string input = Console.ReadLine();
 //int num = -20;
 //bool isNum = int.TryParse(input, out num);
@@ -39,3 +41,54 @@
 //int sum = AddTwoNumbersPlusOne(numOne, ref numTwo);
 //Console.WriteLine(sum);
 //Console.WriteLine("numTwo = " + numTwo);
+
+
+//STRING METHODS
+//string name = "bob";
+//string combinedString = string.Concat("Hey", name);
+//Console.WriteLine(combinedString);
+
+//string name = "bob";
+//Console.WriteLine("bob" == name);
+//Console.WriteLine(name.Equals("bob"));
+//Console.WriteLine(string.Compare(name, "bob"));
+//Console.WriteLine(string.Compare(name, "BOB", true));
+//Console.WriteLine(string.Equals(name, "BoB", StringComparison.OrdinalIgnoreCase));
+
+
+string s = "abc abc abc";
+//Console.WriteLine(s.IndexOf("bc"));
+//Console.WriteLine(s.LastIndexOf("bc"));
+//Console.WriteLine(s.EndsWith("?"));
+
+//string newString = s.Replace("abc", "xyz");
+//Console.WriteLine(newString);
+
+//string[] stringArray = s.Split("b");
+//foreach(string str in stringArray)
+//{
+//    Console.WriteLine(str);
+//}
+
+//string newString = s.Substring(3, 4);
+//Console.WriteLine(newString);
+
+//Console.WriteLine(s.ToUpper());
+
+//string anotherString = "k lkjfd k";
+//Console.WriteLine(anotherString.Trim());
+//Console.WriteLine(anotherString.Trim('k'));
+
+//Console.WriteLine(s.Contains('z'));
+
+
+//Console.WriteLine(s);
+//string string99 = s.Replace("abc", "xyz");
+//Console.WriteLine(s);
+//Console.WriteLine(string99);
+
+
+//StringBuilder sb = new StringBuilder("bob");
+//Console.WriteLine(sb);
+//sb.Append("ABC");
+//Console.WriteLine(sb);
