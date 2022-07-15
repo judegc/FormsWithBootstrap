905229933d9d6f4708c62d68ef7f63ccf9c9e1b6
Aaron Krumsieg <a.krumsieg@gmail.com>
2022-07-14 20:01:42 -0600

    finish dictionaries and add exceptions

--- UnitThree/Collections/Program.cs
+++ UnitThree/Collections/Program.cs
@@ -18,38 +18,38 @@ nums.Insert(1, 123);
 
 List<string> students = new List<string>() { "Michael", "Morgan", "Jacob", "Garret", "Brennan", "Jacob" };
 //for loop with .Count()
-for (int i = 0; i < students.Count(); i++)
-{
-    Console.WriteLine(students[i]);
-}
+//for (int i = 0; i < students.Count(); i++)
+//{
+//    Console.WriteLine(students[i]);
+//}
 
 //.IndexOf()
-int index = students.IndexOf("Jacob");
-Console.WriteLine(index);
+//int index = students.IndexOf("Jacob");
+//Console.WriteLine(index);
 
 //.LastIndexOf()
-Console.WriteLine(students.LastIndexOf("Jacob"));
+//Console.WriteLine(students.LastIndexOf("Jacob"));
 
 //.Sort()
-students.Sort();
-for (int i = 0; i < students.Count(); i++)
-{
-    Console.WriteLine(students[i]);
-}
+//students.Sort();
+//for (int i = 0; i < students.Count(); i++)
+//{
+//    Console.WriteLine(students[i]);
+//}
 
 //.Reverse()
-students.Reverse();
-for (int i = 0; i < students.Count(); i++)
-{
-    Console.WriteLine(students[i]);
-}
+//students.Reverse();
+//for (int i = 0; i < students.Count(); i++)
+//{
+//    Console.WriteLine(students[i]);
+//}
 
 //.Clear()
-students.Clear();
-for (int i = 0; i < students.Count(); i++)
-{
-    Console.WriteLine(students[i]);
-}
+//students.Clear();
+//for (int i = 0; i < students.Count(); i++)
+//{
+//    Console.WriteLine(students[i]);
+//}
 
 
 
@@ -74,9 +74,28 @@ tasty.Add("Steamed Broccoli", false);
 //Console.WriteLine(tasty["Chicken Curry"]);
 
 // new partsCatalog Dictionary<int, string>
+Dictionary<int, string> partsCatalog = new Dictionary<int, string>()
+{
+    { 1001, "hubcap" },
+    { 1002, "lugnuts" },
+    { 1003, "oil filter" }
+};
 
 //.ContainsKey()
+//Console.WriteLine(partsCatalog.ContainsKey(99));
+//Console.WriteLine(partsCatalog.ContainsKey(1002));
 
 //.ContainsValue()
+//Console.WriteLine(partsCatalog.ContainsValue("steering wheel"));
+//Console.WriteLine(partsCatalog.ContainsValue("lugnuts"));
+
+//.ElementAt()
+//KeyValuePair<int, string> firstElement = partsCatalog.ElementAt(0);
+//Console.WriteLine(firstElement);
 
 //.Remove()
+//partsCatalog.Remove(1002);
+//foreach (KeyValuePair<int, string> kvp in partsCatalog)
+//{
+//    Console.WriteLine(kvp);
+//}
---
+++ UnitThree/Exceptions/Program.cs
@@ -0,0 +1,40 @@
+﻿// try-catch blocks
+double subtotal = 0.0;
+
+////topOfTryCatchBlocks:
+//try
+//{
+//    Console.WriteLine("Enter subtotal: ");
+//    subtotal = Double.Parse(Console.ReadLine());
+//}
+//catch (Exception e) //can have multiple catch blocks
+//{
+//    Console.WriteLine("Error! Invalid input.");
+//    Console.WriteLine(e);
+////    goto topOfTryCatchBlocks;
+//}
+//finally
+//{
+//    Console.WriteLine("Finally block is executed.");
+//}
+
+//Console.WriteLine("program has ended");
+
+
+//while (true)
+//{
+//    try
+//    {
+//        Console.WriteLine("Enter a double");
+//        double nber = double.Parse(Console.ReadLine());
+//        break;
+//    }
+//    catch (Exception e)
+//    {
+//        Console.WriteLine(e);
+//        continue;
+//    }
+//}
+
+
+throw new Exception("Input must be less than zero");
