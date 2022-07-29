f5fd16fbd755262e3d91ac91e8da88a51ac6df0a
Aaron Krumsieg <a.krumsieg@gmail.com>
2022-07-28 19:13:57 -0600

    add movie db lab

---
+++ UnitFour/MovieDatabaseLab/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieDatabaseLab
+{
+    public class Movie
+    {
+        public Movie (string title, string category)
+        {
+            Title = title;
+            Category = category;
+        }
+
+        public string Title { get; set; }
+        public string Category { get; set; }
+    }
+}
