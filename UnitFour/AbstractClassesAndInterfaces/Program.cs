c2289214800fa7a79a0d7dab43d0db53f48fe182
Aaron Krumsieg <a.krumsieg@gmail.com>
2022-08-03 19:01:48 -0600

    add another suv

--- UnitFour/AbstractClassesAndInterfaces/Program.cs
+++ UnitFour/AbstractClassesAndInterfaces/Program.cs
@@ -3,6 +3,7 @@
 var taurus = new Sedan("Ford", "Taurus", 5);
 
 var suburban = new SUV("Chevrolet", "Suburban", 7);
+var pilot = new SUV("Honda", "Pilot", 7);
 
 var odyssey = new Minivan("Honda", "Odyssey", 8);
 
