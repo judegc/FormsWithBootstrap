86a96fd7cfb074991519379ce7bea5a3f5a35a36
Aaron Krumsieg <a.krumsieg@gmail.com>
2022-08-08 20:13:14 -0600

    add mock assessment three example

---
+++ UnitFour/MockAssessmentThree/Farmer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockAssessmentThree
+{
+    public class Farmer : Villager
+    {
+        public Farmer()
+        {
+            Hunger = 1;
+        }
+
+        public override int Farm()
+        {
+            return 2;
+        }
+    }
+}
---
+++ UnitFour/MockAssessmentThree/Town.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MockAssessmentThree
+{
+    public class Town
+    {
+        public Town()
+        {
+            Villagers = new List<Villager>()
+            {
+                new Farmer(),
+                new Slacker(),
+                new Slacker(),
+                new Slacker()
+            };
+        }
+
+        public List<Villager> Villagers { get; set; }
+
+        public int Harvest()
+        {
+            int sum = 0;
+            foreach (var villager in Villagers)
+            {
+                sum += villager.Farm();
+            }
+            return sum;
+        }
+
+        public int CalcFoodConsumption()
+        {
+            int sum = 0;
+            foreach (var villager in Villagers)
+            {
+                sum += villager.Hunger;
+            }
+            return sum;
+        }
+
+        public bool SurviveTheWinter()
+        {
+            int harvestTotal = Harvest();
+            int hungerTotal = CalcFoodConsumption();
+            return hungerTotal <= harvestTotal;
+        }
+    }
+}
